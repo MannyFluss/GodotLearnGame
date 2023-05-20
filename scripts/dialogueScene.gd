fc595938cdb88098896e3a7d3b5ac1e735aae715
Manny-OnGit <manny.fluss@gmail.com>
2023-05-20 01:27:43 -0700

    added dialogue

---
+++ scripts/dialogueScene.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+@export var dialogueResource : DialogueResource
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	DialogueManager.show_example_dialogue_balloon(dialogueResource,"Start")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
